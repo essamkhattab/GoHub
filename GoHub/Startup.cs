0abc4c3a2114d312843a0105c14567f8750daf93
Essam Khattab <esamhosnykhatab@gmail.com>
2019-07-24 17:59:26 -0400

    Add project files.

---
+++ GoHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoHub.Startup))]
+namespace GoHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
