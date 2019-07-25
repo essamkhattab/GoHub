33370693daf13ac0087a0e851b5266f053df4072
Essam Khattab <esamhosnykhatab@gmail.com>
2019-07-25 17:03:10 -0400

    Create Add a Go form.

---
+++ GoHub/Controllers/GosController.cs
@@ -0,0 +1,27 @@
+﻿using GoHub.Models;
+using GoHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GoHub.Controllers
+{
+    public class GosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GosController()
+        {
+           _context = new ApplicationDbContext(); 
+        }
+        // GET: Gos
+        public ActionResult Create()
+        {
+            var viewModel = new GoFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ GoHub/ViewModels/GoFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GoHub.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GoHub.ViewModels
+{
+    public class GoFormViewModel
+    {
+        public String Venue { get; set; }
+        public String Date { get; set; }
+        public String Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
+
